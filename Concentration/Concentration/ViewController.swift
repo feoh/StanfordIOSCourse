5201e9fe0b3561a43c8689f2cb9f4f20e339b1f6
Chris Patti <cpatti@gmail.com>
2017-12-18 00:15:09 -0500

    Add more cards, make them flip, add flip counter, and a better arch.

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["👻","🎃","👻","🎃"]
     
     @IBAction func touchCard(_ sender: UIButton) {
-        flipCard(withEmoji: "👻", on: sender)
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+            print("cardNumber = \(cardNumber)")
+        } else {
+            print("chosen card was not in cardButtons")
+        }
+        
     }
     
     func flipCard(withEmoji emoji: String, on Button: UIButton) {
@@ -21,7 +39,7 @@ class ViewController: UIViewController {
             Button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
         }
         else {
-            Button.setTitle("", for: UIControlState.normal)
+            Button.setTitle(emoji, for: UIControlState.normal)
             Button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         }
     }
